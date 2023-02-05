981880d8d261db9f7290461983ddf4fd5712682f
ElectroBoi <64182319+ElectroBoi@users.noreply.github.com>
2023-02-06 03:44:12 +0700

    added a logger and a syncing function

--- FolderAutoBackup.py
+++ FolderAutoBackup.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import numpy as np
 import json
 import zipfile
@@ -12,8 +13,8 @@ from Google import Create_Service
 
 
 #Folder Location
-loc = ''
-zip_name = ' '
+loc = 'Discord Bot'
+zip_name = 'SQL Backup '
 
 
 #Max Stored Zip in Google Drive
@@ -28,25 +29,75 @@ API_NAME = 'drive'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-
-
 drive = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 schedule_list = ["Hourly ", "Daily ", "Monthly "]
 
-with open("ids.json", "r") as f:
-    temp = json.loads(f.read())
-hours = temp[0]
-days = temp[1]
-months = temp[2]
-f.close()
-temp = None
+#create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('[%(asctime)s %(levelname)s] %(message)s','%H:%M:%S - %d/%b/%Y')
+
+file_handler = logging.FileHandler('console.log')
+file_handler.setFormatter(formatter)
+
+steam_handler = logging.StreamHandler()
+steam_handler.setFormatter(formatter)
+
+logger.addHandler(steam_handler)
+logger.addHandler(file_handler)
 
 
 def backup(sched):
+        #open lists of zipped file ids
+    with open("ids.json", "r") as f:
+        ids = json.loads(f.read())
+    
+    
+    #sync json ids to google drive ids
+    logger.info("Syncing Zip file ids to Google Drive")   
+    gdrive_sync = drive.files().list().execute()
+    
+    temp = [[],[],[]]
+    
+    for i in range(len(ids)):
+        logger.debug("for loop i running")
+        for x in range(len(ids[i])):
+            exist = False
+            logger.debug("for loop x running")
+            for j in range(len(gdrive_sync['files'])):
+                logger.debug("for loop j running")
+                if ids[i][x] == gdrive_sync['files'][j]['id']:
+                    temp[i].insert(0, ids[i][x])
+                    logger.debug("THE ONE PIECE IS REAL") 
+                else:
+                    try:
+                        drive.files().delete(fileId = gdrive_sync['files'][j]['id']).execute()
+                    except:
+                        logger.warning("deleted id that does not exist in google drive")
+                    finally:    
+                        logger.warning("deleted files that is not registered locally")
+    
+    ids = list(temp)
+    temp = None
+                
+                    
+    logger.info("Done Syncing!")  
+    
+    
+    #converting 2D Array to 1D Array
+    hours = ids[0]
+    days = ids[1]
+    months = ids[2]
+    f.close()
+    ids = None
+    
+    
     dt = datetime.datetime.now()
     tstamp = dt.strftime("[%H:%M:%S - %d/%b/%Y] ")
     name = schedule_list[sched]
     fname = zip_name + name + '.zip'
+    
     
     #Create Zip
     with zipfile.ZipFile(fname, 'w') as f:
@@ -54,8 +105,8 @@ def backup(sched):
             f.write(file)
         f.close()
         
-        
-    print(tstamp + "Successfully created a " + name + "Backup")
+    logger.info("Successfully created an " + name + "Backup")
+    
     
     #Upload Zip to Google Drive
     file_metadata = {'name': tstamp + fname}
@@ -67,59 +118,76 @@ def backup(sched):
     ).execute()
     
     
+
     
     
+    #adds zipped files id to a list and deleting old ones
     if sched == 0:
         hours.append(file.get('id'))
         while len(hours) > hour:
             drive.files().delete(fileId = hours[0]).execute()
             hours.pop(0)
-            print("one old hourly backup has been deleted")
+            logger.warning("one old hourly backup has been deleted")
             
     elif sched == 1:
         days.append(file.get('id'))
         while len(days) > day:
             drive.files().delete(fileId = days[0]).execute()
             days.pop(0)
-            print("one old daily backup has been deleted")
+            logger.warning("one old daily backup has been deleted")
             
     elif sched == 2:
         months.append(file.get('id'))
         while len(months) > month:
             drive.files().delete(fileId = months[0]).execute()
             months.pop(0)
-            print("one old monthly backup has been deleted")
+            logger.warning("one old monthly backup has been deleted")
     
     
-    
-    
+    #Dumps ids to json file
     f = open("ids.json", "w")
-    temp = [hours, days, months]
-    f.write(json.dumps(temp))
+    ids = [hours, days, months]
+    f.write(json.dumps(ids))
     f.close()
 
     
     if file != {}:
-        print(tstamp + "Successfully uploaded " + name + "Backup to Google Drive")
+        logger.info("Successfully uploaded " + name + "Backup to Google Drive")
         file = {}
     else:
-        Print(tstamp + "Failed to upload " + name + "Backup to Google Drive")
+        logger.error("Failed to upload " + name + "Backup to Google Drive")
         
-        
+    logger.info("Standby..")   
     
 
 
-print("I'm Ready to Backup!")
+logger.info("I'm Ready to Backup!")
 
-
-schedule.every().hour.do(backup, 0)
-schedule.every().day.do(backup, 1)
-schedule.every(30).days.do(backup, 2)
+#schedule
+#schedule.every().minutes.do(backup, 0)
+#schedule.every().day.do(backup, 1)
+#schedule.every(30).days.do(backup,2)
 
 
 
 while True:
     #cmd = input("input 0 / 1 / 2 for hours / days / months : ")
     #backup(int(cmd))    
-    schedule.run_pending()
-    time.sleep(1)
+    #schedule.run_pending() 
+    
+    
+    
+    #python schedule libarary alternative lmao 
+    dt = datetime.datetime.now()
+    if 282020 > int(dt.strftime("%d%H%M")) > 282000:
+        backup(2)
+        time.sleep(1200)
+    elif 2040 > int(dt.strftime("%H%M")) > 2020:
+        backup(1)
+        time.sleep(1200)
+    else:
+        backup(0)
+     
+        
+        
+    time.sleep(3600)
